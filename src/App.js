86b2470637af55d395d0bdacace0cf4e2ae91575
Vitor Moura <jvrezendemoura@gmail.com>
2024-11-20 19:39:08 -0300

    Rmoving speedinsights

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { SpeedInsights } from "@vercel/speed-insights/react"
 import { useState } from 'react';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
@@ -22,7 +21,7 @@ function App() {
         </main>
       </div>
 
-        <Footer/>
+        <Footer/> 
     </>
   );
 }
